b0b78793eaf81ab54654b10bb60b8f31971be22b
Hakim Norizman <30333599+hakimnorizman@users.noreply.github.com>
2020-07-09 16:12:56 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _MyAppState extends State<MyApp> {
 
   Future<void> onClickCmd() async {
     var client = new SSHClient(
-      host: "192.168.8.100",
+      host: "192.168.8.102",
       port: 22,
       username: "admin",
       passwordOrKey: "pfsense",
@@ -32,7 +32,7 @@ class _MyAppState extends State<MyApp> {
     String result;
     try {
       result = await client.connect();
-      if (result == "session_connected") result = await client.execute("ps");
+      if (result == "session_connected") result = await client.execute("ls");
       client.disconnect();
     } on PlatformException catch (e) {
       print('Error: ${e.code}\nError Message: ${e.message}');
@@ -44,38 +44,20 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+//private key tak boleh guna for now
   Future<void> onClickShell() async {
     var client = new SSHClient(
-      host: "192.168.8.100",
+      host: "192.168.8.102",
       port: 22,
-      username: "admin",
+      username: "hakim",
       passwordOrKey: {
         "privateKey": """-----BEGIN RSA PRIVATE KEY-----
-MIIEpAIBAAKCAQEA2DdFSeWG8wOHddRpOhf4FRqksJITr59iXdNrXq+n79QFN1g4
-bvRG9zCDmyLb8EF+gah78dpJsGZVIltmfYWpsk7ok9GT/foCB1d2E6DbEU6mBIPe
-OLxYOqyiea8mi7iGt9BvAB4Mj+v2LnhK4O2BB6PTU4KLjSgMdqtV/EGctLdK+JEU
-5Vo/tDvtF8jmUYGV57V8VovgQJHxOWcT9Mgz+c5EcyVvhwvA/88emYkZ49cn4N0A
-az7YsOTHItvbhxf9xI3bBwxoPvfLx/G0n48TeY33D0qu/qjAloOfqPMHwj+upn/K
-RrAawl2N1MObCc5/q1WYTftd5+uoQsB1RN7ptQIDAQABAoIBAQCzKBkhwi6v7pyv
-5fHLUVEfK5SLOn9VZpv7YtP1AVgGQYiQ82jPh1nGOUzTn27fBWXtyc3p+RZWNHUW
-ouWp3LdgKEJPObmHGUHVE4OjgAYFsUWfOCVKncX92E5IxfkKjTwT04Imdr+yAbNb
-jhF9j077JaRV7jX0INsy+YWmIDfZBQHdR4gpip6ye70yc4p0M7DbrhjEFi6cvf5b
-OaSsbKAunxZte42RYY1ap6GmEii5B/wWe37176jBUrCeQzN9poTSFEv99+Av6M3R
-yyBD1PyawR+dPCAicvIY88ME4fAJSi6Gp8Kmievq7bXnGw8ICWggVSnl0TBYhwSY
-SN8mBr2BAoGBAPNNQ+77kEkwsA0pzZljbwDhJ03jATsWpA4yN4S3Gz456ZUDxode
-lbHERy7RR8l6EunSRdlWGVW9d/8uXBKsvp78hZnJkUE1fLCP+5UH1DVYn+hSYhjj
-g9lnQXbKpXm5tpABiM7+sMq+pC2N6K8yQ7P33TXCcRCWpjK0OJcEVxq/AoGBAOOA
-HNlZe8gQeH3OrQWKEJjgF6oQ9pGdRgJJctdSHDsqP8cPV7BuiYaTh/Q+R+HIueJ+
-3abGLkRqxbNb5FIgX7HJRYLGlusccjd0L4OJ5upGDQJgJzQOryPFofihLvvNbY1K
-zLLNvvYoaWtXhSGusj5N9T6DuA6qxMs+0OwPeZyLAoGBAPHIjwInrTOO1uW97TvJ
-vL47Ajw8ozR9Q3t4HAQfk0s7cg1MOza7oDeQvsyf3Z8zWShUdmWNUpAKQf2trIJC
-eQy2Fm7GCTusU8WC0JlBtnltITxW4nWpY5XhLwVGTTuyeuKRI8vQ/w/8dFtw8xNn
-+DAY2hRartG1ZGRvBO3OumExAoGAeJuar7+417+joU7Ie39OfT2QTiDgFyKB0wSN
-VYm6XcNwPF/t5SM01ZuxH9NE2HZJ1cHcUGYQcUUJuqSkzsVK9j32E/akW9Cg3LVD
-20BooxqwGupO3lJKl3RXAjCxb9zgj19wVfqtmmKiQL4NXmX3KQC7W4EJOv1dh0Ku
-D/fESTECgYBwWv9yveto6pP6/xbR9k/Jdgr+vXQ3BJVU3BOsD38SeSrZfMSNGqgx
-eiukCOIsRHYY7Qqi2vCJ62mwbHJ3RhSKKxcGpgzGX7KoGZS+bb5wb7RGNYK/mVaI
-pFkz72+8eA2cnbWUqHt9WqMUgUBYZTMESzQrTf7+q+0gWf49AZJ/QQ==
+AAAAB3NzaC1yc2EAAAABJQAAAQEA3WoHFYtAv/pgA0VRxsroIy4VzRK0+SGRvCNh
+MwMvuLHLVkrNPnKW0HyPup8ygxFXXIsREM5ZzsiIh0qe3NwX82oT3HMyt+Ef4Aej
+rk+QW/PBLbDERZ36EaLd+8pFLn2MxM6p1asGVrwgAUmndoyYDmUJba38xGKU/+EC
+ZMpVYn3eV+/IvuwX+m5TMvQyNeug1VXRGpoHi7EjCKnET1xG0QKX0gHtUXKlAGAT
+9esidEnOMvv5L52BMxkRvyUrgd6IO3FmCaiRKFc0T12ZdGh5RemdztcGB3rgcqvS
+VhpY85fOcDVFBQTvnxKpgxKJccXusVyhJ1hMxB9wZU9dogAk6w==
 -----END RSA PRIVATE KEY-----""",
       },
     );
@@ -112,7 +94,7 @@ pFkz72+8eA2cnbWUqHt9WqMUgUBYZTMESzQrTf7+q+0gWf49AZJ/QQ==
 
   Future<void> onClickSFTP() async {
     var client = new SSHClient(
-      host: "192.168.8.100",
+      host: "192.168.8.102",
       port: 22,
       username: "admin",
       passwordOrKey: "pfsense",
